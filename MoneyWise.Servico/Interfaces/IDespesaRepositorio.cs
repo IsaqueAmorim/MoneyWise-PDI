32b996b4b6f06c7021731fa194639477e8498cdb
André Messias <andremessias.az@gmail.com>
2023-09-23 00:08:38 -0300

    adicionado base para repositorio da classe despesa

---
+++ MoneyWise.Servico/Interfaces/IDespesaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using MoneyWise.Dominio.Entidades;
+
+namespace MoneyWise.Servico.Interfaces
+{
+    public interface IDespesaRepositorio
+    {
+        List<Despesa> PegarTodasPorIdUsuario(Guid idUsuario);
+        List<Despesa> PegarTodasNoPeriodo(Guid idUsuario, DateTime dataInicial, DateTime dataFinal);
+        List<Despesa> PegarTodasPorValor(Guid idUsuario, decimal valor);
+        void Incluir(params Despesa[] despesaList);
+        void Atualizar(params Despesa[] despesaList);
+        void Remover(params Despesa[] despesaList);
+    }
+}
