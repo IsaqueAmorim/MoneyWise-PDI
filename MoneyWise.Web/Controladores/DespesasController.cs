f8479ded762516816e8eff7b25f76074e5d065c1
André Messias <andremessias.az@gmail.com>
2023-11-11 15:03:21 -0300

    🐉 Implementado Controladores de Despesas

---
+++ MoneyWise.Web/Controladores/DespesasController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyWise.Dominio.Entidades;
+using MoneyWise.Servico.Despesas;
+
+namespace MoneyWise.Web.Controladores
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DespesasController : ControllerBase
+    {
+        private readonly ServicoDespesa _repositorio;
+        public DespesasController(ServicoDespesa repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            try
+            {
+                return Ok(_repositorio.ObterTodos().ToList());
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao obter as despesas. {erro}");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ObterPorPeriodo(DateTime dataInicial, DateTime dataFinal)
+        {
+            try
+            {
+                return Ok(_repositorio.ObterDespesasPorPeriodo(dataInicial, dataFinal));
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao obter as despesas. {erro}");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ObterPorValor(decimal valor)
+        {
+            try
+            {
+                return Ok(_repositorio.ObterDespesasPorValor(valor));
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao obter as despesas. {erro}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(Guid id)
+        {
+            try
+            {
+                return Ok(_repositorio.ObterPorId(id));
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao obter despesa por id {id}. {erro}");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Despesa despesa)
+        {
+            try
+            {
+                if (despesa == null) return BadRequest();
+
+                _repositorio.Adicionar(despesa);
+
+                return Created($"usuarios/{despesa.Id}", despesa);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest($"Erro ao criar usuário :: {erro.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar([FromBody] Despesa despesa)
+        {
+            try
+            {
+                if (despesa == null) return BadRequest();
+
+                _repositorio.Atualizar(despesa);
+
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao atualizar usuário. {erro}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(Despesa despesa)
+        {
+            try
+            {
+                if (despesa == null) return BadRequest();
+
+                _repositorio.Remover(despesa);
+
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return NotFound($"Erro ao deletar usuário. {erro}");
+            }
+        }
+    }
+}
